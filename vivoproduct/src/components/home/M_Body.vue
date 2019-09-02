68bd88c6c775869285da14009c752676665763df
xueyang <915989176@qq.com>
2019-09-02 18:28:57 +0800

    更新

--- vivoproduct/src/components/home/M_Body.vue
+++ vivoproduct/src/components/home/M_Body.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <imgs></imgs>
+    <div class="img">
+        <div style="margin-right:10px;">
+          <img src="../../assets/图片1.jpg" alt="">
+        </div>
+        <div>
+          <img src="../../assets/图片2.jpg" alt="">
+        </div>
+    </div>
   </div>
 </template>
 <script>
@@ -27,5 +35,15 @@ export default {
 }
 </script>
 <style scoped>
-
+.img{
+  display:flex;
+width:100%;
+}
+.img>div{
+  width:50%;
+  box-sizing: border-box;
+}
+.img>div>img{
+  width:100%;
+}
 </style>
