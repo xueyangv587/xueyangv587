990382ccd1e2172ba2d0a35fec5aa5590873f282
xueyang <915989176@qq.com>
2019-09-05 14:07:38 +0800

    更新

--- vivoproduct/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from '../node_modules/axios'
-
-Vue.config.productionTip = false
-Vue.prototype.axios=axios
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
